20f6d4503d145cda556f61e56dd0b7b759766954
getfunctional <mail@getfunctional.de>
2019-12-01 18:56:30 +0100

    System1

--- Couno/Couno/Engine/Card.cs
+++ Couno/Couno/Engine/Card.cs
@@ -1,7 +1,126 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Windows.Media.Converters;
+using Couno.Shared;
 
 namespace Couno.Engine
 {
+    public class AbilityActionBuilder
+    {
+
+        IAbilityAction Create( Ability primaryAbility, Stack<Ability> ancestors, Queue<Ability> descendants)
+
+
+    }
+
+
+    public class AbilityCombiner
+    {
+        AbilityActionsQueue CreateActionsFromAbilityQueue(Queue<Ability> abilities)
+        {
+            var abilityActionsQueue = new AbilityActionsQueue();
+            var untouchedAbilities = new Queue<Ability>(abilities);
+            var touchedAbilities = new Stack<Ability>();
+
+            while (untouchedAbilities.Peek() != null)
+            {
+                var dequeuedAbility = untouchedAbilities.Dequeue();
+
+
+
+                touchedAbilities.Push(dequeuedAbility);
+            }
+
+            return abilityActionsQueue;
+        }
+        
+
+
+    }
+
+
+    public class AbilityEvaluator
+    {
+
+        AbilityActionsQueue Evaluate(IAbilityToken abilityToken)
+        {
+            var abilities = ExtractAbilities(abilityToken);
+
+
+            foreach (var ability in abilities)
+            {
+
+             
+                
+            }
+        }
+
+        private Queue<Ability> ExtractAbilities(IAbilityToken abilityToken)
+        {
+            CodeGuard.ArgumentNotNull(abilityToken, nameof(abilityToken));
+            var abilityQueue = new Queue<Ability>();
+
+            foreach (var ability in abilityToken.Abilities)
+            {
+                abilityQueue.Enqueue(ability);
+            }
+
+            if (abilityToken.Descendant != null)
+            {
+                var descendantAbilities = ExtractAbilities(abilityToken.Descendant);
+                foreach (var descendantAbility in descendantAbilities)
+                {
+                    abilityQueue.Enqueue(descendantAbility);
+                }
+            }
+
+            return abilityQueue;
+        }
+    }
+
+    internal interface IAbilityToken
+    {
+        IAbilityToken Ancestor { get; }
+
+        IAbilityToken Descendant { get; }
+
+        IList<Ability> Abilities { get; }
+    }
+
+    internal class Ability
+    {
+        int Amount { get; }
+
+        string Name { get; }
+
+        private AbilityType AbilityType { get; }
+    }
+
+    internal enum AbilityType
+    {
+        None = 0,
+        Attack = 1,
+        Block = 2,
+        TakeAncestor = 3,
+        TakeDescendant = 4,
+    }
+
+    public class AbilityActionsQueue
+    {
+        private Queue<IAbilityAction> Actions { get; }
+
+    }
+
+    internal interface IAbilityAction
+    {
+        Queue<IAbilityToken> AbilityTokens { get; }
+
+
+    }
+
+
     public class Card
     {
         public string Name { get; }
@@ -12,7 +131,7 @@ namespace Couno.Engine
     public class Character
     {
 
-        public string Name { get;  }
+        public string Name { get; }
 
         public int Life { get; private set; }
 
@@ -39,7 +158,7 @@ namespace Couno.Engine
 
     public class Player
     {
-        public CardDeck DrawDeck { get;  }
+        public CardDeck DrawDeck { get; }
 
         public CardDeck DiscardPile { get; }
 
