8c150824cf0f60b77738da7e1c2dc18753462631
getfunctional <mail@getfunctional.de>
2019-12-29 19:08:33 +0100

    TestLibary and Core created

---
+++ Couno2/GF.Couno/GF.Couno.Core.Unittests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GF.Couno.Core.Unittests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GF.Couno.Core.Unittests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a35fc07e-eddf-4a3d-9ee9-37355c5b52e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
